24688d51990ad405d8b9547859d8c835967a4e93
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-18 22:53:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(highlight="*"){
+    return function(adj = "special") {
+        return `You are ${highlight}${adj}${highlight}!`
+    }
+}
+
+let Calculator = {
+    add(a,b){
+        return a+b
+    },
+    subtract(a,b){
+       return a-b
+    },
+    multiply(a,b){
+       return a*b
+    },
+    divide(a,b){
+       return a/b
+    }
+}
+
+function actionApplyer(start, arr){
+    if (arr.length === 0){
+        return start
+    }
+    else{
+        return (((start*2)+1000)%7)
+    }
+}
